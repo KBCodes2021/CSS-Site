9fe475fee29f48f424a70a09f451292d57393306
Nick Krause <nkrause@gmail.com>
2021-11-04 19:28:37 -0700

    first commit

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 
-//jshint esversion:6
-
-const express = require("express");
-
-const bodyParser = require("body-parser");
-
-const app = express();
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
-
-app.get("/", function(req, res){
-  res.sendFile(__dirname + "/index.html");
-});
 
 
+ //jshint esversion:6
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server is running on port 3000");
-});
+ const express = require("express");
+
+ const bodyParser = require("body-parser");
+
+ const app = express();
+
+ app.use(bodyParser.urlencoded({extended: true}));
+ app.use(express.static("public"));
+
+
+ app.get("/", function(req, res){
+   res.sendFile(__dirname + "/index.html");
+ });
+
+
+ app.listen(process.env.PORT || 3000, function() {
+   console.log("Server is running on port 3000");
+ });
